c5a8a69491e2b21f3a9fa16717be45ef9a3ff9a8
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-10-16 21:06:49 +0530

    Create variables.tf
    Initial commit

---
+++ variables.tf
@@ -0,0 +1,53 @@
+variable "region" {
+  type = string
+}
+variable "cidr" {
+  type = string
+}
+variable "name" {
+  type = string
+}
+variable "project" {
+  type = string
+}
+variable "tower" {
+  type = string
+}
+variable "owner" {
+  type = string
+}
+variable "mail" {
+  type = string
+}
+variable "billing" {
+  type = string
+}
+variable "pub_cidr" {
+  type = string
+}
+variable "pub_name" {
+  type = string
+}
+
+variable "priv_cidr" {
+  type = string
+}
+
+variable "priv_name" {
+  type = string
+}
+variable "igw" {
+  type = string
+}
+variable "pri-nat-ip" {
+  type = string
+}
+variable "nat" {
+  type = string
+}
+variable "rt" {
+  type = string
+}
+variable "privrt" {
+  type = string
+}
